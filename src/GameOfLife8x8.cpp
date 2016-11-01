2631b5cb431ff9ed2ade35eb8800960fec941070
Enrico Gueli <enrico.gueli@gmail.com>
2016-11-01 16:43:36 +0100

    mutate each cell of the generated array, then print difference in start time and period

--- src/GameOfLife8x8.cpp
+++ src/GameOfLife8x8.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <sstream>
+#include <iomanip>
 
 #include "field.h"
 #include "algo.h"
@@ -7,8 +8,11 @@
 template<size_t Cols, size_t Rows>
 void print_field(const Field<Cols, Rows> & field);
 
-Field<8, 8> random_field();
+void print_8x8_array(array< array<int, 8>, 8> &arr);
 
+
+
+Field<8, 8> random_field();
 
 int main(int argc, char *argv[]) {
 	unsigned int r_seed;
@@ -48,6 +52,36 @@ int main(int argc, char *argv[]) {
 
 	cout << "period start: " << start_time << endl;
 	cout << "period length: " << period << endl;
+	cout << endl;
+
+	array< array<int, 8>, 8> mut_start = {};
+	array< array<int, 8>, 8> mut_period = {};
+	for (int y = 0; y < 8; y++) {
+		for (int x = 0; x < 8; x++) {
+			Field<8, 8> mutated = seed;
+			mutated.set(x, y, !mutated.get(x, y));
+			int m_start;
+			int m_period;
+			algo.run(mutated, m_start, m_period);
+			mut_start[x][y] = m_start - start_time;
+			mut_period[x][y] = m_period - period;
+		}
+	}
+
+
+	cout << "start time variations:" << endl;
+	print_8x8_array(mut_start);
+	cout << "period variations:" << endl;
+	print_8x8_array(mut_period);
+}
+
+void print_8x8_array(array< array<int, 8>, 8> &arr) {
+	for (int y = 0; y < 8; y++) {
+		for (int x = 0; x < 8; x++) {
+			cout << setw(4) << arr[x][y] << " ";
+		}
+		cout << endl;
+	}
 }
 
 
